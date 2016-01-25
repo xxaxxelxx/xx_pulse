29f32cc515c204e9ac43f50d92871135624630bd
xxaxxelxx <x@axxel.net>
2016-01-25 12:26:02 +0100

    updated

--- pulse.sh
+++ pulse.sh
@@ -30,6 +30,7 @@ while true; do
 
     if [ "x$CPULOAD" != "x" -a "x$IOLOAD" != "x" -a "x$MOUNT" != "x" ]; then
 	    curl -o /dev/null --connect-timeout 1 --digest --user "admin:$ADMIN_PASS" -s "http://$LOADBALANCER_ADDR/update.php?mnt=$MOUNT&bw=$IOLOAD&bwl=$BW_LIMIT&load=$CPULOAD&loadl=$LOAD_LIMIT" 2>&1 > /dev/null && echo "$CPULOAD" > /host/tmp/pulse.cpuload
+	    echo "$LOADBALANCER_ADDR" > /host/tmp/loadbalancer.addr
     fi
     sleep $LOOP_SEC
 done
